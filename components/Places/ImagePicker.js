15b167f7ac2be0daaf5bc8061fe4b94b0824c6af
Wallace Torres <Wallace.DaSilvaTorres@campus-support.ca>
2025-01-17 16:50:28 -0800

    Taking pictures

---
+++ components/Places/ImagePicker.js
@@ -0,0 +1,71 @@
+import { Alert, Button, Image, View, Text, StyleSheet } from 'react-native';
+import { launchCameraAsync, useCameraPermissions, PermissionStatus } from 'expo-image-picker';
+import { useState } from 'react';
+import { Colors } from '../../constants/colors';
+
+function ImagePicker() {
+	const [pickedImage, setPickedImage] = useState();
+	const [camerPermissionInformation, requestPermission] = useCameraPermissions();
+
+	async function verifyPermission() {
+		if (camerPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+			const permissionResponse = await requestPermission();
+
+			return permissionResponse.granted;
+		}
+
+		if (camerPermissionInformation.status === PermissionStatus.DENIED) {
+			Alert.alert('Insufficient Permission!', 'You need to grant camera permission to use this app.');
+			return false;
+		}
+
+		return true;
+	}
+
+	async function takeImageHandler() {
+		const hasPermission = await verifyPermission();
+
+		if (!hasPermission) {
+			return;
+		}
+
+		const image = await launchCameraAsync({
+			allowsEditing: true,
+			aspect: [16, 9],
+			quality: 0.5,
+		});
+		console.log(image);
+		setPickedImage(image.assets[0].uri);
+	}
+
+	let imagePreview = <Text>No image taken yet.</Text>;
+
+	if (pickedImage) {
+		imagePreview = <Image source={{ uri: pickedImage }} style={styles.image} />;
+	}
+
+	return (
+		<View>
+			<View style={styles.imagePreview}>{imagePreview}</View>
+			<Button title='Take picture' onPress={takeImageHandler} />
+		</View>
+	);
+}
+
+export default ImagePicker;
+
+const styles = StyleSheet.create({
+	imagePreview: {
+		width: '100%',
+		height: 200,
+		marginVertical: 8,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: Colors.primary100,
+		borderRadius: 4,
+	},
+	image: {
+		width: '100%',
+		height: '100%',
+	},
+});
