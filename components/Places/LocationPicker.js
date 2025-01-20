2e11c2752cd75c6a65e87846596c277038bf4b52
Wallace Torres <Wallace.DaSilvaTorres@campus-support.ca>
2025-01-20 11:11:39 -0800

    Image preview

--- components/Places/LocationPicker.js
+++ components/Places/LocationPicker.js
@@ -1,14 +1,59 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, Alert, Image, Text } from 'react-native';
+import { getCurrentPositionAsync, useForegroundPermissions, PermissionStatus } from 'expo-location';
+
 import OutlinedButton from '../UI/OutlinedButton';
 import { Colors } from '../../constants/colors';
+import { useState } from 'react';
+import { getMapPreview } from '../../util/location';
 
 const LocationPicker = () => {
-	function getLocationHandler() {}
+	const [pickedLocation, setPickedLocation] = useState();
+	const [locationPermissionInformation, requestPermission] = useForegroundPermissions();
+
+	async function verifyPermissions() {
+		console.log('Current location permission status:', locationPermissionInformation.status);
+
+		if (locationPermissionInformation.status === PermissionStatus.UNDETERMINED) {
+			const permissionResponse = await requestPermission();
+			console.log('Permission response:', permissionResponse);
+			return permissionResponse.granted;
+		}
+
+		if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+			Alert.alert('Insufficient Permission!', 'You need to grant location permission to use this app.');
+			return false;
+		}
+
+		return true;
+	}
+
+	async function getLocationHandler() {
+		const hasPermission = await verifyPermissions();
+
+		if (!hasPermission) {
+			return;
+		}
+
+		const location = await getCurrentPositionAsync();
+		setPickedLocation({
+			lat: location.coords.latitude,
+			lng: location.coords.longitude,
+		});
+	}
+
 	function pickOnMapHandler() {}
+
+	let locationPreview = <Text>No location picked yet.</Text>;
+
+	if (pickedLocation) {
+		locationPreview = (
+			<Image style={styles.image} source={{ uri: getMapPreview(pickedLocation.lat, pickedLocation.lng) }} />
+		);
+	}
 
 	return (
 		<View>
-			<View style={styles.mapPreview}></View>
+			<View style={styles.mapPreview}>{locationPreview}</View>
 			<View style={styles.actions}>
 				<OutlinedButton icon='location' onPress={getLocationHandler}>
 					Locate User
@@ -37,5 +82,9 @@ const styles = StyleSheet.create({
 		flexDirection: 'row',
 		justifyContent: 'space-around',
 		alignItems: 'center',
+	},
+	image: {
+		width: '100%',
+		height: '100%',
 	},
 });
